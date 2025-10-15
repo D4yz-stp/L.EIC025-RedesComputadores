e8132f8b4b946216bcd3b6e73739f501f8ab29a7
Divaldo Dias <164824282+D4yz-stp@users.noreply.github.com>
2025-10-15 15:15:23 +0100

    Applation_Layer defined to send String to llopen

--- src/application_layer.c
+++ src/application_layer.c
@@ -4,8 +4,37 @@
 
 #include <stdio.h>
 
+typedef struct
+{
+    char serialPort[50];
+    LinkLayerRole role;
+    int baudRate;
+    int nRetransmissions;
+    int timeout;
+} LinkLayer;
+typedef enum
+{
+    LlTx,
+    LlRx,
+} LinkLayerRole;
+
+
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename)
 {
-    // TODO: Implement this function
-}
+    char Text[] = "Ola";
+    
+    LinkLayerRole roleLink;
+    roleLink = (strcmp(role == "tx")) ? LlTx : LlRx;
+    
+    LinkLayer linkLayer = {
+        .role = roleLink,
+        .baudRate = baudRate,
+        .nRetransmissions = nTries,
+        .timeout = timeout
+    };
+    strncpy(linkLayer.serialPort, serialPort, 50);
+    linkLayer.serialPort[49] = '\0';
+
+    int correct_Open = llopen(linkLayer);
+}   
