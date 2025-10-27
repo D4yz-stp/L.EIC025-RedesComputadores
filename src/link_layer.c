777ac7aab09ef21c8c539ec470ef34cd0fcc0230
root <root@DivaldoMachine.>
2025-10-27 16:54:03 +0000

    llclose implemented and small corrections

--- src/link_layer.c
+++ src/link_layer.c
@@ -40,6 +40,12 @@ extern int fd;
 extern int alarmEnabled;
 extern int alarmCount;
 
+// Global variables of the connection and roles
+static LinkLayerRole globalRole;
+static int globalTimeout;
+static int globalNRetransmissions;
+
+
 // State machine
 enum State_SU{START, FLAG_RCV, A_RCV, C_RCV, BCC1_OK, STOP};
 
@@ -112,8 +118,11 @@ int writeToSerialPort(unsigned char *frame, int frameSize, int timeout, int *nRe
             fprintf(stderr, "Erro: falha ao escrever frame (%d/%d bytes)\n", bytesWritten, frameSize);
             return -1;
         }
+
+        /*
         printf("Frame sent. Waiting for response... (retransmissions left: %d)\n", *nRetransmissions);
-        
+        */
+
         alarm(timeout);
         alarmEnabled = TRUE;
         return 0;
@@ -133,6 +142,10 @@ int llopen(LinkLayer connectionParameters)
         perror("openSerialPort");
         return -1;
     }
+
+    globalRole = connectionParameters.role;
+    globalTimeout = connectionParameters.timeout;
+    globalNRetransmissions = connectionParameters.nRetransmissions;
     
     if (connectionParameters.role == LlTx) {
         if (setupAlarm() < 0) {
@@ -144,8 +157,8 @@ int llopen(LinkLayer connectionParameters)
     if (connectionParameters.role == LlTx) {
         printf("TX: Sending SET frame...\n");
         
-        unsigned char frameTx[SUFrame_SIZE];
-        buildSUFrame(frameTx, A_TX, C_SET);
+        unsigned char setFrame[SUFrame_SIZE];
+        buildSUFrame(setFrame, A_TX, C_SET);
         
         int nRetransmissions = connectionParameters.nRetransmissions;
         int timeout = connectionParameters.timeout;
@@ -157,7 +170,7 @@ int llopen(LinkLayer connectionParameters)
         alarmCount = 0;
         
         while (nRetransmissions > 0) {
-            writeToSerialPort(frameTx, SUFrame_SIZE, timeout, &nRetransmissions);
+            writeToSerialPort(setFrame, SUFrame_SIZE, timeout, &nRetransmissions);
             
             state = START;
             
@@ -249,10 +262,10 @@ int llopen(LinkLayer connectionParameters)
         
         printf("RX: SET received. Sending UA...\n");
         
-        unsigned char frameTx[SUFrame_SIZE];
-        buildSUFrame(frameTx, A_RX, C_UA);
+        unsigned char uaFrame[SUFrame_SIZE];
+        buildSUFrame(uaFrame, A_RX, C_UA);
         
-        if (writeBytesSerialPort(frameTx, SUFrame_SIZE) < 0) {
+        if (writeBytesSerialPort(uaFrame, SUFrame_SIZE) < 0) {
             perror("writeBytesSerialPort - UA");
             return -1;
         }
@@ -276,9 +289,9 @@ int llwrite(const unsigned char *buf, int bufSize)
         return -1;
     }
     
-    int nRetransmissions = 3;
-    int timeout = 3;
-    
+    int nRetransmissions = globalNRetransmissions;
+    int timeout = globalTimeout;
+
     enum State_SU state = START;
     unsigned char byte;
     unsigned char expectedRR = (Ns == 0) ? C_RR1 : C_RR0;
@@ -413,8 +426,8 @@ int llread(unsigned char *packet)
                                 unsigned char rrControl = (Nr == 0) ? C_RR1 : C_RR0;
                                 buildSUFrame(rrFrame, A_RX, rrControl);
                                 int bytesWritten = writeBytesSerialPort(rrFrame, SUFrame_SIZE);
-                                if (bytesWritten != frameSize) {
-                                    fprintf(stderr, "Erro: falha ao escrever frame (%d/%d bytes)\n", bytesWritten, frameSize);
+                                if (bytesWritten != SUFrame_SIZE) {
+                                    fprintf(stderr, "Erro: falha ao escrever frame (%d/%d bytes)\n", bytesWritten, SUFrame_SIZE);
                                     return -1;
                                 }
 
@@ -457,8 +470,8 @@ int llread(unsigned char *packet)
                             
                             buildSUFrame(rrFrame, A_RX, rrControl);
                             int bytesWritten = writeBytesSerialPort(rrFrame, SUFrame_SIZE);
-                            if (bytesWritten != frameSize) {
-                                fprintf(stderr, "Erro: falha ao escrever frame (%d/%d bytes)\n", bytesWritten, frameSize);
+                            if (bytesWritten != SUFrame_SIZE) {
+                                fprintf(stderr, "Erro: falha ao escrever frame (%d/%d bytes)\n", bytesWritten, SUFrame_SIZE);
                                 return -1;
                             }
                             
@@ -470,8 +483,8 @@ int llread(unsigned char *packet)
                             unsigned char rejControl = (Nr == 0) ? C_REJ0 : C_REJ1;
                             buildSUFrame(rejFrame, A_RX, rejControl);
                             int bytesWritten = writeBytesSerialPort(rejFrame, SUFrame_SIZE);
-                            if (bytesWritten != frameSize) {
-                                fprintf(stderr, "Erro: falha ao escrever frame (%d/%d bytes)\n", bytesWritten, frameSize);
+                            if (bytesWritten != SUFrame_SIZE) {
+                                fprintf(stderr, "Erro: falha ao escrever frame (%d/%d bytes)\n", bytesWritten, SUFrame_SIZE);
                                 return -1;
                             }
                             
@@ -515,14 +528,180 @@ int llread(unsigned char *packet)
 // LLCLOSE
 //===============================================
 
+/*
+    globalRole = connectionParameters.role;
+    globalTimeout = connectionParameters.timeout;
+    globalNRetransmissions = connectionParameters.nRetransmissions;
+*/
 int llclose()
 {
-    // TODO: Implement DISC/UA handshake
-    
-    if (closeSerialPort() < 0) {
-        perror("closeSerialPort");
-        exit(-1);
+    if( globalRole == LlTx ){
+        printf("Tx: Preparing to send Disc ( SU Frame) to RX\n");
+        unsigned char discFrame[SUFrame_SIZE];
+        buildSUFrame(discFrame, A_TX, C_DISC);
+
+        int nRetransmissions = globalNRetransmissions;
+        int timeout = globalTimeout;
+
+        enum State_SU state = START;
+        unsigned char byte;
+
+        alarmEnabled = FALSE;
+        alarmCount = 0;
+        
+        while (nRetransmissions > 0) {
+            writeToSerialPort(discFrame, SUFrame_SIZE, timeout, &nRetransmissions);
+            printf("Tx: Disc ( SU Frame ) Sent\n");
+            
+            state = START;
+            
+            while (state != STOP && alarmEnabled) {
+                if (readByteSerialPort(&byte) > 0) {
+                    switch(state) {
+                        case START:
+                            if (byte == FLAG) state = FLAG_RCV;
+                            break;
+                        case FLAG_RCV:
+                            if (byte == FLAG) state = FLAG_RCV;
+                            else if (byte == A_RX) state = A_RCV;
+                            else state = START;
+                            break;
+                        case A_RCV:
+                            if (byte == FLAG) state = FLAG_RCV;
+                            else if (byte == C_DISC) state = C_RCV;
+                            else state = START;
+                            break;
+                        case C_RCV:
+                            if (byte == FLAG) state = FLAG_RCV;
+                            else if (byte == (A_RX ^ C_DISC)) state = BCC1_OK;
+                            else state = START;
+                            break;
+                        case BCC1_OK:
+                            if (byte == FLAG) {
+                                state = STOP;
+                                alarm(0);
+                                alarmEnabled = FALSE;
+                                printf("TX: Disc received from RX.\n");
+
+                                printf("TX: Preparring UA ( SU frame ) to finish the connection.\n");
+
+                                unsigned char UAFrame[SUFrame_SIZE];
+                                buildSUFrame( UAFrame, A_TX, C_UA);
+                                writeBytesSerialPort( UAFrame, SUFrame_SIZE);
+                                
+                                close(fd);
+
+                                printf("Tx: Connection terminated\n");
+
+                                return 0;
+                            }
+                            else state = START;
+                            break;
+                        case STOP:
+                            break;
+                    }
+                }
+            }
+            
+            if (!alarmEnabled) {
+                nRetransmissions--;
+                printf("TX: Timeout or REJ! Retransmitting...\n");
+            }
+        }
+        
+        printf("TX: ERROR - Failed to establish connection after all retries.\n");
+        closeSerialPort();
+        return -1;
+        
     }
-    printf("Connection closed.\n");
-    return 0;
+    else{
+        printf("RX: Waiting for DISC frame...\n");
+        
+        enum State_SU state = START;
+        unsigned char byte;
+        
+        while (state != STOP) {
+            if (readByteSerialPort(&byte) > 0) {
+                switch(state) {
+                    case START:
+                        if (byte == FLAG) state = FLAG_RCV;
+                        break;
+                    case FLAG_RCV:
+                        if (byte == FLAG) state = FLAG_RCV;
+                        else if (byte == A_TX) state = A_RCV;
+                        else state = START;
+                        break;
+                    case A_RCV:
+                        if (byte == FLAG) state = FLAG_RCV;
+                        else if (byte == C_DISC) state = C_RCV;
+                        else state = START;
+                        break;
+                    case C_RCV:
+                        if (byte == FLAG) state = FLAG_RCV;
+                        else if (byte == (A_TX ^ C_DISC)) state = BCC1_OK;
+                        else state = START;
+                        break;
+                    case BCC1_OK:
+                        if (byte == FLAG) state = STOP;
+                        else state = START;
+                        break;
+                    case STOP:
+                        break;
+                }
+            }
+        }
+        
+        printf("RX: DISC received. Sending DISC...\n");
+        
+        unsigned char discFrame[SUFrame_SIZE];
+        buildSUFrame(discFrame, A_RX, C_DISC);
+        
+        if (writeBytesSerialPort(discFrame, SUFrame_SIZE) < 0) {
+            perror("writeBytesSerialPort - UA");
+            return -1;
+        }
+
+        printf("RX: Waiting for UA frame...\n");
+        
+        state = START;
+        
+        while (state != STOP) {
+            if (readByteSerialPort(&byte) > 0) {
+                switch(state) {
+                    case START:
+                        if (byte == FLAG) state = FLAG_RCV;
+                        break;
+                    case FLAG_RCV:
+                        if (byte == FLAG) state = FLAG_RCV;
+                        else if (byte == A_TX) state = A_RCV;
+                        else state = START;
+                        break;
+                    case A_RCV:
+                        if (byte == FLAG) state = FLAG_RCV;
+                        else if (byte == C_UA) state = C_RCV;
+                        else state = START;
+                        break;
+                    case C_RCV:
+                        if (byte == FLAG) state = FLAG_RCV;
+                        else if (byte == (A_TX ^ C_UA)) state = BCC1_OK;
+                        else state = START;
+                        break;
+                    case BCC1_OK:
+                        if (byte == FLAG) state = STOP;
+                        else state = START;
+                        break;
+                    case STOP:
+                        break;
+                }
+            }
+        }
+
+        printf("RX: UA received. Terminating the connection...\n");
+
+        close(fd);
+
+        
+        return 0;
+    }
+
 }
